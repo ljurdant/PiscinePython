9510abf0db3a5ca80054702e595e1704959840e4
Louis JURDANT <ljurdant@studebt.42.fr>
2022-08-29 18:09:00 +0200

    removed examples from ex10

--- Module00/ex10/loading.py
+++ Module00/ex10/loading.py
@@ -40,11 +40,3 @@ def ft_progress(lst):
         width = os.get_terminal_size()[0]
         print(message, end="\r")
         yield lst[i]
-
-listy = range(1000)
-ret = 0
-for elem in ft_progress(listy):
-    ret += (elem + 3) % 5
-    sleep(0.01)
-print()
-print(ret)
