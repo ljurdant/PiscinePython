77eba650894e1e23ea01c770e142cac5c4ad95df
Louis JURDANT <ljurdant@studebt.42.fr>
2022-08-31 14:02:13 +0200

    final corrections to MOdule00

--- Module00/ex01/exec.py
+++ Module00/ex01/exec.py
@@ -4,10 +4,16 @@ args = sys.argv
 for i in range(len(sys.argv) - 1, 0, -1):
 	words = args[i].split()
 	words.reverse()
+	lsts = []
 	for word in words:
-		for j in range(len(word) - 1, -1, -1):
-			if word[j].islower():
-				print(word[j].upper(), end='')
-			else:
-				print(word[j].lower(), end='')
-		print(' ', end='')
+		lst = []
+		for letter in word:
+			if letter.isupper():
+				letter = letter.lower()
+			elif letter.islower():
+				letter = letter.upper()
+			lst.append(letter)
+		lst.reverse()
+		word = ''.join(lst)
+		lsts.append(word)
+	print(' '.join(lsts), end=(' ', '\n')[i == 1])
--- Module00/ex05/kata00.py
+++ Module00/ex05/kata00.py
@@ -1,5 +1,5 @@
 # Put this at the top of your kata00.py file
-kata = (19,42,21)
+kata = (1, 2 ,3, 6, 7)
 
 
 print("The",len(kata),"numbers are:",", ".join([str(k) for k in kata]))
